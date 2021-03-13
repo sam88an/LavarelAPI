54eb2809de6bd04c6d7b8ca0186823be906999db
sam88an@gmail.com <sam88an@gmail.com>
2021-03-13 14:55:55 +0700

    Product paginate

--- routes/api.php
+++ routes/api.php
@@ -18,7 +18,9 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('products', 'ProductController');
+
+
+Route::apiResource('/products', 'ProductController');
 Route::group(['prefix' => 'products'], function () {
-    Route::apiResource('/{products}/reviews', 'ReviewController');
+    Route::apiResource('/{product}/reviews', 'ReviewController');
 });
