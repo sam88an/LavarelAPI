bd65acf8f9dc5e73259d939af7672c95f1bb6b63
sam88an@gmail.com <sam88an@gmail.com>
2021-03-13 14:23:30 +0700

    Product collection

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -9,20 +9,21 @@ class ProductResource extends JsonResource
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request
      * @return array
      */
     public function toArray($request)
     {
         return [
             'name' => $this->name,
-            'descreption' => $this->detail,
+            'description' => $this->detail,
             'price' => $this->price,
-            'stock' => $this->stock == 0 ? 'Out of stock' : $this->stock,
+            'stock' => $this->stock == 0 ? 'Out of Stock' : $this->stock,
             'discount' => $this->discount,
-            'ratting' => $this->reviews->count() > 0 ? round($this->reviews->sum('star') / $this->reviews->count(), 2) : 'No ratting yet...',
+            'totalPrice' => round((1 - ($this->discount / 100)) * $this->price, 2),
+            'rating' => $this->reviews->count() > 0 ? round($this->reviews->sum('star') / $this->reviews->count(), 2) : 'No rating yet',
             'href' => [
-                'review' => route('reviews.index', $this->id)
+                'reviews' => route('reviews.index', $this->id)
             ]
         ];
     }
